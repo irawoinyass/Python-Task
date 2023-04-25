64a522cb746b71e7079d7056d4f2f1b45136aa14
irawoinyass <lasisisaheed5@gmail.com>
2023-04-25 16:22:03 +0100

    moving accounts.serializer codes to post.serialers

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,6 +1,39 @@
 from rest_framework import serializers
-from accounts.serializers import NestedCommentPublicSerializer, PostPublicSerializer, WhoCommentPublicSerializer, WhoPostedPublicSerializer, CategoryPublicSerializer, PrimaryCommentPublicSerializer
 from .models import Post, Comment, NestedComment
+
+
+class WhoCommentPublicSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(read_only=True)
+
+
+class NestedCommentPublicSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    comment = serializers.CharField(read_only=True)
+    created_at = serializers.DateField(read_only=True)
+
+
+class PostPublicSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(read_only=True)
+    body = serializers.CharField(read_only=True)
+    posted_at = serializers.DateField(read_only=True)
+
+
+class WhoPostedPublicSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(read_only=True)
+
+
+class CategoryPublicSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    category_name = serializers.CharField(read_only=True)
+
+
+class PrimaryCommentPublicSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    comment = serializers.CharField(read_only=True)
+    created_at = serializers.DateField(read_only=True)
 
 
 class PostSerializer(serializers.ModelSerializer):
